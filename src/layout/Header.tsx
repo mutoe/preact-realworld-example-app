492cbe2cbd2c8fd9c0b7916e52528c1efacbeb17
mutoe <mutoe@foxmail.com>
2019-11-28 20:46:08 +0800

    refactor: change Header.tsx to functional component

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,47 +1,46 @@
-import { Component, Fragment, h } from 'preact'
+import { Fragment, h } from 'preact'
 import { Link } from 'preact-router'
 import { useContext } from 'preact/hooks'
 import RootStateContext from '../stores/globalContext'
 
-export default class Header extends Component {
-  render() {
-    const rootState = useContext(RootStateContext)
+const Header = () => {
+  const rootState = useContext(RootStateContext)
 
-    return <nav className="navbar navbar-light">
-      <div className="container">
-        <a className="navbar-brand" href="/">conduit</a>
-        <ul className="nav navbar-nav pull-xs-right">
-          <li className="nav-item">
-            <Link className="nav-link" activeClassName="active" href="/">Home</Link>
-          </li>
-          {
-            rootState.user ? (
-              <Fragment>
-                <li className="nav-item">
-                  <a className="nav-link" href="/settings">
-                    <i className="ion-compose" /> New Post
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" activeClassName="active" href="/settings">
-                    <i className="ion-gear-a" /> Settings
-                  </Link>
-                </li>
-              </Fragment>
-            ) : (
-              <Fragment>
-                <li className="nav-item">
-                  <Link className="nav-link" activeClassName="active" href="/register">Sign up</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" activeClassName="active" href="/login">Sign in</Link>
-                </li>
-              </Fragment>
-            )
-          }
-        </ul>
-      </div>
-    </nav>
-  }
+  return <nav className="navbar navbar-light">
+    <div className="container">
+      <a className="navbar-brand" href="/">conduit</a>
+      <ul className="nav navbar-nav pull-xs-right">
+        <li className="nav-item">
+          <Link className="nav-link" activeClassName="active" href="/">Home</Link>
+        </li>
+        {
+          rootState.user ? (
+            <Fragment>
+              <li className="nav-item">
+                <a className="nav-link" href="/settings">
+                  <i className="ion-compose" /> New Post
+                </a>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" activeClassName="active" href="/settings">
+                  <i className="ion-gear-a" /> Settings
+                </Link>
+              </li>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <li className="nav-item">
+                <Link className="nav-link" activeClassName="active" href="/register">Sign up</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" activeClassName="active" href="/login">Sign in</Link>
+              </li>
+            </Fragment>
+          )
+        }
+      </ul>
+    </div>
+  </nav>
 }
 
+export default Header
