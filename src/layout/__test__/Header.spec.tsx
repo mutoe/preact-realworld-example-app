77125ed01f20bd2b410682928f3bb96351b24825
mutoe <mutoe@foxmail.com>
2019-11-28 20:37:29 +0800

    test: update Header.test.tsx up snapshot

--- src/layout/__test__/Header.spec.tsx
+++ src/layout/__test__/Header.spec.tsx
@@ -1,8 +1,8 @@
-import { mount } from 'enzyme'
 import { h } from 'preact'
 import { useContext } from 'preact/hooks'
 import { initialRootState } from '../../stores/globalContext'
 import Header from '../Header'
+import render from 'preact-render-to-string'
 
 jest.mock('preact/hooks', () => ({
   useContext: jest.fn(() => initialRootState),
@@ -17,23 +17,17 @@ describe('# Header Component', () => {
     (useContext as jest.Mock).mockReturnValue({
       user: null,
     })
-    const wrapper = mount(<Header />)
+    const html = render(<Header />)
 
-    expect(wrapper.findWhere(n => n.type() === 'a' && /Sign in/.test(n.text()))).toHaveLength(1)
-    expect(wrapper.findWhere(n => n.type() === 'a' && /Sign up/.test(n.text()))).toHaveLength(1)
-    expect(wrapper.findWhere(n => n.type() === 'a' && /Settings/.test(n.text()))).toHaveLength(0)
-    expect(wrapper.findWhere(n => n.type() === 'a' && /New Post/.test(n.text()))).toHaveLength(0)
+    expect(html).toMatchSnapshot()
   })
 
   it('should display post and settings and hidden logging buttons when logged', () => {
     (useContext as jest.Mock).mockReturnValue({
       user: {},
     })
-    const wrapper = mount(<Header />)
+    const html = render(<Header />)
 
-    expect(wrapper.findWhere(n => n.type() === 'a' && /Sign in/.test(n.text()))).toHaveLength(0)
-    expect(wrapper.findWhere(n => n.type() === 'a' && /Sign up/.test(n.text()))).toHaveLength(0)
-    expect(wrapper.findWhere(n => n.type() === 'a' && /Settings/.test(n.text()))).toHaveLength(1)
-    expect(wrapper.findWhere(n => n.type() === 'a' && /New Post/.test(n.text()))).toHaveLength(1)
+    expect(html).toMatchSnapshot()
   })
 })
