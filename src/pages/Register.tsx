755749f72835f5282d2c0067255dabce5aaffe03
mutoe <mutoe@foxmail.com>
2019-12-02 21:53:18 +0800

    feat(register): add disabled button when one item is not filled in

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -22,6 +22,18 @@ export default class Register extends Component<{}, RegisterState> {
     return
   }
 
+  onInputUsername(username: string) {
+    this.setState({ username })
+  }
+
+  onInputEmail(email: string) {
+    this.setState({ email })
+  }
+
+  onInputPassword(password: string) {
+    this.setState({ password })
+  }
+
   render() {
     return (
       <div className="auth-page">
@@ -44,15 +56,29 @@ export default class Register extends Component<{}, RegisterState> {
 
               <form>
                 <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="text" placeholder="Your Name" />
+                  <input className="form-control form-control-lg"
+                    type="text"
+                    placeholder="Your Name"
+                    value={this.state.username}
+                    onInput={e => this.onInputUsername(e.currentTarget.value)} />
                 </fieldset>
                 <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="text" placeholder="Email" />
+                  <input className="form-control form-control-lg"
+                    type="text"
+                    placeholder="Email"
+                    onInput={e => this.onInputEmail(e.currentTarget.value)}
+                    value={this.state.email} />
                 </fieldset>
                 <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="password" placeholder="Password" />
+                  <input className="form-control form-control-lg"
+                    type="password"
+                    placeholder="Password"
+                    onInput={e => this.onInputPassword(e.currentTarget.value)}
+                    value={this.state.password} />
                 </fieldset>
-                <button className="btn btn-lg btn-primary pull-xs-right" onClick={this.onRegister}>
+                <button className="btn btn-lg btn-primary pull-xs-right"
+                  disabled={!(this.state.email && this.state.username && this.state.password)}
+                  onClick={this.onRegister}>
                   Sign up
                 </button>
               </form>
