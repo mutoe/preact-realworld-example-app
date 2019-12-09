57cfb72092ec59ec2e345d352cd50fefb025da32
mutoe <mutoe@foxmail.com>
2019-12-09 15:46:47 +0800

    style: eslint fix

--- src/setup-test.ts
+++ src/setup-test.ts
@@ -1,34 +1,36 @@
 // This `<reference ...>` directive is necessary to include the adapter's
 // extensions to types in the "preact" and "enzyme" packages.
 
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="enzyme-adapter-preact-pure"/>
 
-import {  JSDOM } from 'jsdom'
+import { JSDOM } from 'jsdom'
 
-import { configure } from "enzyme"
-import Adapter from "enzyme-adapter-preact-pure"
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-preact-pure'
 
 const dom = new JSDOM('', {
-  pretendToBeVisual: true
+  pretendToBeVisual: true,
 })
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace NodeJS {
     interface Global {
-      Event: typeof Event
-      Node: typeof Node
+      Event: typeof Event;
+      Node: typeof Node;
       document: Document;
       window: Window;
       navigator: Navigator;
-      requestAnimationFrame: AnimationFrameProvider['requestAnimationFrame']
+      requestAnimationFrame: AnimationFrameProvider['requestAnimationFrame'];
     }
   }
 }
 
-global.Event = dom.window.Event;
-global.Node = dom.window.Node;
-global.window = dom.window;
-global.document = dom.window.document;
-global.requestAnimationFrame = dom.window.requestAnimationFrame;
+global.Event = dom.window.Event
+global.Node = dom.window.Node
+global.window = dom.window
+global.document = dom.window.document
+global.requestAnimationFrame = dom.window.requestAnimationFrame
 
 configure({ adapter: new Adapter() })
