646c3e73d5fa4364edae1e1dca2edf02caac6eb2
mutoe <mutoe@foxmail.com>
2019-12-16 15:04:58 +0800

    feat: jump to login page when 401 returned

--- src/__test__/index.spec.tsx
+++ src/__test__/index.spec.tsx
@@ -6,6 +6,7 @@ jest.mock('preact-router')
 
 describe('# Root Component', function () {
   it('should be jump Login page when request 401 code got', async function () {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (axios.interceptors.response as any).handlers[0].rejected({
       response: { status: 401 },
     })
