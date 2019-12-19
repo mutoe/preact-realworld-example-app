b4fecdce595f2cb9c76cbdad3bfe975fdc49575f
mutoe <mutoe@foxmail.com>
2019-12-19 11:26:51 +0800

    test: fix Register tests

--- src/pages/__test__/Register.spec.tsx
+++ src/pages/__test__/Register.spec.tsx
@@ -1,12 +1,24 @@
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import { h } from 'preact'
 import { postRegister } from '../../services'
 import Register from '../Register'
+import { route } from 'preact-router'
 
 jest.mock('../../services')
 jest.mock('preact-router')
 
-const postRegisterMock = postRegister as jest.Mock
+const postRegisterMock = postRegister as jest.Mock<Promise<UserWithToken>>
+
+const mockResolvedPostRegister = () => {
+  postRegisterMock.mockResolvedValue({
+    id: 1,
+    email: 'test@example.com',
+    username: 'test',
+    bio: null,
+    image: null,
+    token: 'foobar',
+  })
+}
 
 afterEach(() => {
   jest.clearAllMocks()
@@ -42,89 +54,80 @@ describe('# Register error message', () => {
   })
 })
 
-// describe('# Register form validate', () => {
-//   it('should set button disabled when submit a empty form field', function () {
-//     const wrapper = shallow(<Register />)
-//     const loginButton = wrapper.find('form button.btn-lg.btn-primary')
-//     console.log(wrapper.find('input[type="email"]').renderProp)
-//     wrapper.find('input[type="email"]').getDOMNode<HTMLInputElement>().value = '123'
-//
-//     expect(loginButton.props().disabled).toBe(true)
-//   })
-// })
+describe('# Register form validate', () => {
+  it('should set button disabled when submit a empty form field', function () {
+    const wrapper = mount(<Register />)
+    wrapper.find('input[type="email"]').getDOMNode<HTMLInputElement>().value = '123'
+    wrapper.update()
 
-// describe('# Register request', () => {
-//   it('should be send form when sign in button clicked', function () {
-//     const wrapper = shallow(<Register />)
-//     instance.onRegister = jest.fn()
-//     instance.forceUpdate()
-//     const loginButton = wrapper.find('form button.btn-lg.btn-primary')
-//
-//     loginButton.simulate('click')
-//
-//     expect(instance.onRegister).toBeCalled()
-//   })
-//
-//   it('can set error messages correctly when received error response', async function () {
-//     postRegisterMock.mockImplementation(() => Promise.reject({
-//       errors: { 'email and password': [ 'is invalid' ] },
-//     }))
-//     wrapper.setState({
-//       email: 'bad_account@example.com',
-//       password: '123456',
-//     })
-//
-//     await instance.onRegister()
-//
-//     expect(postRegister).toBeCalled()
-//     expect(wrapper.state().errors).toHaveProperty('email and password')
-//   })
-//
-//   it('should not be send when given invalid form', function () {
-//     wrapper.setState({
-//       username: '123',
-//       email: '123',
-//       password: '123',
-//     })
-//     wrapper.update()
-//     const registerButton = wrapper.find('form button.btn-lg.btn-primary')
-//
-//     registerButton.simulate('click')
-//
-//     expect(postRegister).not.toBeCalled()
-//   })
-//
-//   it('should can goto home page when entering the correct account', async function () {
-//     postRegisterMock.mockImplementation(() => Promise.resolve({ token: 'foobar' }))
-//     wrapper.setState({
-//       username: 'test_user',
-//       email: 'test@example.com',
-//       password: '123456',
-//     })
-//
-//     await instance.onRegister()
-//
-//     expect(route).toBeCalledWith('/')
-//   })
-//
-//   it('should save token locally when register successful', async function () {
-//     (postRegister as jest.Mock<Promise<UserWithToken>>).mockResolvedValue({
-//       id: 1,
-//       email: 'test@example.com',
-//       username: 'test',
-//       bio: null,
-//       image: null,
-//       token: 'foobar',
-//     })
-//     jest.spyOn(window.localStorage, 'setItem')
-//     wrapper.setState({
-//       email: 'test@example.com',
-//       password: '123456',
-//     })
-//
-//     await instance.onRegister()
-//
-//     expect(window.localStorage.setItem).toBeCalledWith('token', 'foobar')
-//   })
-// })
-//
+    const submitButton = wrapper.find('form button.btn-lg.btn-primary')
+    expect(submitButton.props().disabled).toBe(true)
+  })
+
+  it('should not be send when given invalid form', function () {
+    const wrapper = mount(<Register />)
+    wrapper.find('input[placeholder="Email"]').getDOMNode<HTMLInputElement>().value = '123'
+    wrapper.find('input[placeholder="Your Name"]').getDOMNode<HTMLInputElement>().value = '123'
+    wrapper.find('input[placeholder="Password"]').getDOMNode<HTMLInputElement>().value = '123'
+    wrapper.find('form button.btn-lg.btn-primary').simulate('click')
+
+    expect(postRegister).not.toBeCalled()
+  })
+})
+
+describe('# Register request', () => {
+  it('should be send form when sign up button clicked', function () {
+    const wrapper = mount(<Register />)
+    wrapper.find('input[placeholder="Email"]').getDOMNode<HTMLInputElement>().value = 'test@example.com'
+    wrapper.find('input[placeholder="Your Name"]').getDOMNode<HTMLInputElement>().value = 'test'
+    wrapper.find('input[placeholder="Password"]').getDOMNode<HTMLInputElement>().value = '12345678'
+
+    wrapper.find('form button.btn-lg.btn-primary').simulate('click')
+
+    expect(postRegister).toBeCalledTimes(1)
+  })
+
+  it('can set error messages correctly when received error response', async function () {
+    postRegisterMock.mockRejectedValue({
+      errors: { 'email and password': [ 'is invalid' ] },
+    })
+    const wrapper = mount(<Register />)
+    wrapper.find('input[placeholder="Email"]').getDOMNode<HTMLInputElement>().value = 'bad_account@example.com'
+    wrapper.find('input[placeholder="Your Name"]').getDOMNode<HTMLInputElement>().value = 'test'
+    wrapper.find('input[placeholder="Password"]').getDOMNode<HTMLInputElement>().value = '12345678'
+
+    wrapper.find('form button.btn-lg.btn-primary').simulate('click')
+    expect(postRegister).toBeCalled()
+    await new Promise(r => setImmediate(r))
+
+    expect(wrapper.find('.error-messages').text()).toContain('email and password')
+  })
+
+  it('should can goto home page when entering the correct account', async function () {
+    mockResolvedPostRegister()
+    const wrapper = mount(<Register />)
+    wrapper.find('input[placeholder="Email"]').getDOMNode<HTMLInputElement>().value = 'test@example.com'
+    wrapper.find('input[placeholder="Your Name"]').getDOMNode<HTMLInputElement>().value = 'test'
+    wrapper.find('input[placeholder="Password"]').getDOMNode<HTMLInputElement>().value = '12345678'
+
+    wrapper.find('form button.btn-lg.btn-primary').simulate('click')
+    await new Promise(r => setImmediate(r))
+
+    expect(route).toBeCalledWith('/')
+  })
+
+  it('should save token locally when register successful', async function () {
+    mockResolvedPostRegister()
+    const wrapper = mount(<Register />)
+    jest.spyOn(window.localStorage, 'setItem')
+    wrapper.find('input[placeholder="Email"]').getDOMNode<HTMLInputElement>().value = 'test@example.com'
+    wrapper.find('input[placeholder="Your Name"]').getDOMNode<HTMLInputElement>().value = 'test'
+    wrapper.find('input[placeholder="Password"]').getDOMNode<HTMLInputElement>().value = '12345678'
+
+    wrapper.find('form button.btn-lg.btn-primary').simulate('click')
+    await new Promise(r => setImmediate(r))
+
+    expect(window.localStorage.setItem).toBeCalledWith('token', 'foobar')
+  })
+})
+
