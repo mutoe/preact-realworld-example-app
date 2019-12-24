ff5d45b6e79bada1770b62a3c1f291e775e8dbd2
mutoe <mutoe@foxmail.com>
2019-12-24 13:33:56 +0800

    test: supplement service tests

--- src/__test__/services.spec.ts
+++ src/__test__/services.spec.ts
@@ -1,12 +1,37 @@
 import FetchRequest from '../utils/request'
-import { postArticle } from '../services'
+import { getArticles, postArticle, postLogin, postRegister } from '../services'
+import { generateArticles } from '../utils/test-utils'
 
 afterEach(() => {
   jest.clearAllMocks()
 })
 
 describe('# Service', function () {
-  test('## create article', async function () {
+
+  test('login', async function () {
+    const form = {
+      email: 'test@example.com',
+      password: '12345678',
+    }
+    jest.spyOn(FetchRequest.prototype, 'post').mockResolvedValue({ user: form })
+    const user = await postLogin(form)
+
+    expect(user).toMatchObject(form)
+  })
+
+  test('register', async function () {
+    const form = {
+      email: 'test@example.com',
+      password: '12345678',
+      username: 'test',
+    }
+    jest.spyOn(FetchRequest.prototype, 'post').mockResolvedValue({ user: form })
+    const user = await postRegister(form)
+
+    expect(user).toMatchObject(form)
+  })
+
+  test('create article', async function () {
     const form = {
       title: 'title',
       description: 'description',
@@ -17,5 +42,14 @@ describe('# Service', function () {
     const article = await postArticle(form)
 
     expect(article).toMatchObject(form)
+  })
+
+  test('get article', async function () {
+    const articles = generateArticles(2)
+    jest.spyOn(FetchRequest.prototype, 'get').mockResolvedValue({ articles, articlesCount: 2 })
+    const result = await getArticles()
+
+    expect(result).toHaveProperty('articles')
+    expect(result).toHaveProperty('articlesCount')
   })
 })
