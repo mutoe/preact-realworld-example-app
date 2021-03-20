c4580f67eb72affcc39693adbd1da576bb08294f
Ryan Christian <ryanchristian4427@gmail.com>
2021-03-20 02:19:07 -0500

    fix: Broken eslint assertion

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,4 @@ import { h, render } from 'preact'
 
 import App from './App'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 render(<App />, document.querySelector('#app')!)
