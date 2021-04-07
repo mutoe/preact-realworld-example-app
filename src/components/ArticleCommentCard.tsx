d6dcdce73c41cecd8e409f6c15c9bf9d78f5586d
Ryan Christian <ryanchristian4427@gmail.com>
2021-04-06 21:07:21 -0500

    refactor: Article Comment card

--- src/components/ArticleCommentCard.tsx
+++ src/components/ArticleCommentCard.tsx
@@ -1,21 +1,24 @@
 import { h } from 'preact';
 
+import { apiDeleteComment } from '../services/api/comments';
 import useStore from '../store';
 import { DEFAULT_AVATAR } from '../utils/constants';
 import { dateFormatter } from '../utils/dateFormatter';
 
 interface ArticleCommentCardProps {
+	articleSlug: string;
 	comment: ArticleComment;
-	onDelete?: (commentId: number) => void;
+	onDelete: (commentId: number) => void;
 }
 
 export default function ArticleCommentCard(props: ArticleCommentCardProps) {
-	const { comment, onDelete } = props;
+	const { comment } = props;
 	const user = useStore(state => state.user);
 
-	const isOwnComment = user?.username === comment.author.username;
-
-	if (!comment.author) return null;
+	const onDelete = async () => {
+		await apiDeleteComment(props.articleSlug, comment.id);
+		props.onDelete(comment.id);
+	};
 
 	return (
 		<div class="card">
@@ -32,8 +35,7 @@ export default function ArticleCommentCard(props: ArticleCommentCardProps) {
 				</a>
 				<span class="date-posted">{dateFormatter(comment.createdAt)}</span>
 				<span class="mod-options">
-					{/* <i class="ion-edit" /> */}
-					{isOwnComment && <i class="ion-trash-a" onClick={() => onDelete?.(comment.id)} />}
+					{user?.username === comment.author.username && <i class="ion-trash-a" onClick={onDelete} />}
 				</span>
 			</div>
 		</div>
