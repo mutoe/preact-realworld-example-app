24366401b46b580ee68ed35b7423fb786153770e
Ryan Christian <ryanchristian4427@gmail.com>
2021-04-07 22:34:50 -0500

    fix: Needed to change store function form to work with jest mock

--- src/components/AuthErrorHandler.tsx
+++ src/components/AuthErrorHandler.tsx
@@ -3,12 +3,14 @@ import { h } from 'preact';
 import useStore from '../store';
 
 export default function AuthErrorHandler() {
-	const error = useStore(state => state.error);
+	const { error } = useStore(state => ({
+		error: state.error
+	}));
 
 	return !error ? null : (
 		<ul class="error-messages">
 			{Object.keys(error).map(key => (
-				<li key={key}>
+				<li key={key} aria-label={`${key} error`}>
 					{key} {error[key]}
 				</li>
 			))}
